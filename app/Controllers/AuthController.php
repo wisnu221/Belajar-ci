6e1e9de2f38946d01d3053f6da4465170b536f30
wisnu221 <priambadhaadhitya@gmail.com>
2025-04-15 19:02:34 +0700

    Tambah halaman login

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AuthController extends BaseController
+{
+    function __construct()
+    {
+        helper('form');
+    }
+
+    public function login()
+    {
+        if ($this->request->getPost()) {
+            $username = $this->request->getVar('username');
+            $password = $this->request->getVar('password');
+
+            $dataUser = ['username' => 'wisnu', 'password' => '827ccb0eea8a706c4c34a16891f84e7b', 'role' => 'admin']; // passw 12345
+
+            if ($username == $dataUser['username']) {
+                if (md5($password) == $dataUser['password']) {
+                    session()->set([
+                        'username' => $dataUser['username'],
+                        'role' => $dataUser['role'],
+                        'isLoggedIn' => TRUE
+                    ]);
+
+                    return redirect()->to(base_url('/'));
+                } else {
+                    session()->setFlashdata('failed', 'Username & Password Salah');
+                    return redirect()->back();
+                }
+            } else {
+                session()->setFlashdata('failed', 'Username Tidak Ditemukan');
+                return redirect()->back();
+            }
+        } else {
+            return view('v_login');
+        }
+    }
+    
+        public function logout()
+    {
+        session()->destroy();
+        return redirect()->to('login');
+    }
+}
