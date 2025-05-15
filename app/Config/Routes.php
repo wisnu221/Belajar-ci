0c8f485b4c3dc822742b0ffd94b8a81588aab647
wisnu221 <priambadhaadhitya@gmail.com>
2025-05-15 15:31:08 +0700

    .

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,5 @@ $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
-
 $routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
