d151ff72e6fe0342b16aa24030372cb5fc7c487e
wisnu221 <priambadhaadhitya@gmail.com>
2025-05-23 18:30:57 +0700

    Create,edit & delete

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,4 +13,8 @@ $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
 $routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->post('produk', 'ProdukController::create', ['filter' => 'auth']);
+$routes->post('produk/edit/(:any)', 'ProdukController::edit/$1', ['filter' => 'auth']);
+$routes->get('produk/delete/(:any)', 'ProdukController::delete/$1', ['filter' => 'auth']);
+
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
